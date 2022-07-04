ed82f79595a0f5d066ed16e56e2d75c907a18112
Vladimir V. Bychkov <github@bychkov.name>
2022-07-04 20:47:25 +0200

    Initial revision

---
+++ src/main/java/name/bychkov/test/IndexPage.java
@@ -0,0 +1,16 @@
+package name.bychkov.test;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class IndexPage implements Serializable
+{
+	public void doException()
+	{
+		throw new RuntimeException();
+	}
+}
